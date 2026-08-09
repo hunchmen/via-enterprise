0240f0961ab1d77edf98155e55c72054b3d6d0e4
Valentin Alberto <valalberto0214@gmail.com>
2026-08-09 13:09:40 +0800

    chore: update spring.session.jdbc database name

--- ems-api/src/test/java/com/via/ems/EmsApiApplicationTests.java
+++ ems-api/src/test/java/com/via/ems/EmsApiApplicationTests.java
@@ -2,8 +2,29 @@ package com.via.ems;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.session.Session;
+import org.springframework.session.SessionRepository;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+@SpringBootTest(
+        properties = {
+            "spring.datasource.url=jdbc:h2:mem:emsdb;MODE=MySQL;DB_CLOSE_DELAY=-1",
+            "spring.datasource.driver-class-name=org.h2.Driver",
+            "spring.datasource.username=sa",
+            "spring.datasource.password=",
+            "spring.jpa.hibernate.ddl-auto=create-drop",
+            "spring.jpa.show-sql=false",
+            "spring.session.jdbc.initialize-schema=always",
+            "spring.session.jdbc.table-name=SPRING_SESSION",
+            "debug=false"
+        })
+@AutoConfigureMockMvc
 class EmsApiApplicationTests {
 
     @Test
